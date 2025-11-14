90b6283fb95e4ac5dd92b27bc7aaf5ae7d346bce
Ranjeet-Sahu07 <23BET10386@cuchd.in>
2025-11-14 21:41:41 +0530

    Add Course.java for Part A - Spring DI

---
+++ PartA_SpringDI/src/main/java/com/example/di/Course.java
@@ -0,0 +1,38 @@
+package com.example.di;
+
+public class Course {
+    private String courseName;
+    private int duration;
+
+    public Course() {
+    }
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", duration=" + duration +
+                " hours}";
+    }
+}
